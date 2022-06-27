5783bde07b114edbbe5c9a8e041f4afc4d146ce5
marekSurak <marek.surak@gmail.com>
2022-06-27 11:12:11 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Moon } from "./components/Moon";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Moon />
+    </div>
+  );
+}
---
+++ src/components/Moon/index.jsx
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+export const Moon = () => (
+  <div className="moon-container">
+    <img className="moon" src="/img/moon.svg" alt="Moon" />
+  </div>
+);
